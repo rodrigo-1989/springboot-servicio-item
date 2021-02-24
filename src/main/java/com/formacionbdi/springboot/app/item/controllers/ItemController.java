c13392d5c5b4da430f081e80b0fa4643b67d6ed1
rodrigo <rodrigo_l321@hotmail.com>
2021-02-24 02:09:03 -0500

    Agregado de servicio de Configuraciones

--- src/main/java/com/formacionbdi/springboot/app/item/controllers/ItemController.java
+++ src/main/java/com/formacionbdi/springboot/app/item/controllers/ItemController.java
@@ -8,6 +8,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,15 +22,21 @@ import com.formacionbdi.springboot.app.item.models.Producto;
 import com.formacionbdi.springboot.app.item.models.service.ItemService;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
+@RefreshScope
 @RestController
 public class ItemController {
     
     private static Logger log = LoggerFactory.getLogger(ItemController.class);
+
+    @Autowired
+    private Environment env;
     
     @Autowired
     @Qualifier("serviceFeign")
     private ItemService itemService;
-    
+
+    @Value("${configuracion.texto}")
+    private String texto;
     @GetMapping("/listar")
     public List<Item> listar(){
         return itemService.findAll();
@@ -49,6 +58,20 @@ public class ItemController {
         producto.setPrecio(500.00);
         item.setProducto(producto);
         return item;
-        
+    }
+
+    @GetMapping("/obtener-config")
+    public ResponseEntity<?>obtenerConfig (@Value("${server.port}") String puerto){
+        log.info(texto);
+        Map<String,String> json = new HashMap<>();
+        json.put("texto",texto);
+        json.put("puerto",puerto);
+
+        if (env.getActiveProfiles().length >0 && env.getActiveProfiles()[0].equals("dev")){
+            json.put("autor.nombre",env.getProperty("configuracion.autor.nombre"));
+            json.put("autor.email",env.getProperty("configuracion.autor.email"));
+        }
+
+        return new ResponseEntity<Map<String,String>>(json, HttpStatus.OK);
     }
 }
